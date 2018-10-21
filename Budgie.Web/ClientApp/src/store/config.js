cc234e95cdaf49f0f6fe8fa49421506d3d9774ce
Dave Russell <dave@daveajrussell.com>
2018-10-21 16:26:09 +0100

    delete things that should be .gitignored

--- Budgie.Web/ClientApp/src/store/config.js
+++ Budgie.Web/ClientApp/src/store/config.js
@@ -1,3 +1,9 @@
+const isDevelopment = process.env.NODE_ENV === 'development';
+let apiUrl = '';
+
+if (isDevelopment) apiUrl = 'https://localhost:5001';
+else apiUrl = 'https://budgiereact.azurewebsites.net';
+
 export const config = {
-    apiUrl: 'https://localhost:5001'
+	apiUrl: apiUrl
 };
