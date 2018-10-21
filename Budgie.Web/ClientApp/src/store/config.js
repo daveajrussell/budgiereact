d47e1ab0f3b43c1ada8493abd3cf9375331c7f7b
Dave Russell <dave@daveajrussell.com>
2018-10-21 16:36:21 +0100

    add .vscode gitignore and update config

--- Budgie.Web/ClientApp/src/store/config.js
+++ Budgie.Web/ClientApp/src/store/config.js
@@ -1,7 +1,7 @@
 const isDevelopment = process.env.NODE_ENV === 'development';
 let apiUrl = '';
 
-if (isDevelopment) apiUrl = 'https://localhost:5001';
+if (isDevelopment) apiUrl = 'http://localhost:50098';
 else apiUrl = 'https://budgiereact.azurewebsites.net';
 
 export const config = {
